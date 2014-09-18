5168f0c7312edcf22c11638e64d24672d59b8627
agibson <art@embed.ly>
2014-09-18 16:11:25 -0400

    youtube search api

---
+++ ona-workshop/google.js
@@ -0,0 +1,47 @@
+/**
+ * Helps us generate an access token for Google OAUTH2.0
+ * Ref: https://github.com/google/google-api-nodejs-client
+ * This file is used for manual creation of OAUTH AccessToken.
+**/
+var google = require('googleapis'),
+  config = require('./config.js');
+
+
+/** Global google **/
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var oauth2Client = new OAuth2(config.google.clientId, config.google.clientSecret, config.google.redirectURL);
+google.options({ auth: oauth2Client }); // set auth as a global default
+
+
+/** Enter code from visited auth_url below **/
+var code = null;
+
+/** Generate AUTH URL to get code **/
+if (code == null) {
+
+  /* Generate a url that asks permissions for Youtube scope */
+  var scopes = [
+    'https://www.googleapis.com/auth/youtube',
+  ];
+
+  var auth_url = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+    scope: scopes// If you only need one scope you can pass it as string
+  });
+
+  console.log("Visit this URL with a google account to get code: " + auth_url);
+
+}
+
+/** Spit out access token **/
+oauth2Client.getToken(code, function(err, tokens, body) {
+  // Now tokens contains an access_token and an optional refresh_token. Save them.
+  if(!err) {
+    console.log("Update access or refresh tokens: " + tokens);
+    oauth2Client.setCredentials(tokens);
+  }
+  else{
+    console.log('Get token Error: ' + err)
+  }
+});
---
+++ ona-workshop/youtube.js
@@ -0,0 +1,43 @@
+var google = require('googleapis'),
+  config = require('./config.js');
+
+
+/** Global google **/
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var oauth2Client = new OAuth2(config.google.clientId, config.google.clientSecret, config.google.redirectURL);
+google.options({ auth: oauth2Client }); // set auth as a global default
+
+/** Set Credentials **/
+oauth2Client.setCredentials({
+  access_token: config.google.accessToken
+});
+
+
+/** Youtube V3 API Access **/
+var youtube = google.youtube('v3');
+
+/*
+ * Search for Youtube video urls.
+ *
+ * @param  {string} query - search query allows boolean OR(|) and NOT(|)
+ * @param  {string} order - order by parameter acceptable values: date, rating, relevance (default), title, videoCount, viewCount.
+ * @param  {int} maxResults - 0 to 50 results, Default: 5
+ * @param {function} cb - callback to print out URLS.
+ */
+var search = function(query, order, maxResults, cb) {
+  var videos = youtube.search.list({'part': 'id', 'q': query, 'order': order}, function(err, videos) {
+  if (err) {
+    console.log('youtube.search.list Error: ' + err.message);
+  }
+  else {
+    videos.items.forEach(function(entry) {
+    cb('http://www.youtube.com/watch?v=' + entry.id.videoId);
+    });
+  }
+  });
+};
+
+/** Run this ish **/
+//search('cats', 'date', 5, console.log);
+
