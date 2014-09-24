854ebab0c646c1821f3780c95ff50b71693f6d76
agibson <art@embed.ly>
2014-09-24 00:10:43 -0400

    update config

--- ona-workshop/config.js
+++ ona-workshop/config.js
@@ -6,7 +6,7 @@ exports.twitter={consumerKey:'wQetyiUi65QzfYroJlUw',
 exports.google={clientId:'947404691893-1t17e89b714idjvokt1mbqul88qtssnu.apps.googleusercontent.com',
   clientSecret:'58G-J_lDchfqIxkE2ErIYFr5',
   redirectURL: 'http://localhost.com:8003/oauth2callback',
-  accessToken : 'ya29.iQDiQSD8WSHarLIP5xarKD0h85kAHTLFaKNj-TF3lm9u3A9xI6xM9QYw',
+  accessToken : 'ya29.igBF71Bv9MoSF9RmxZ5I9bpq3RcDphKTFQUusiOSIQ0MjJhS-o27Qukm',
   accessTokenSecret:''};
 
 exports.nytimes={searchKey:'c3a851d61a8e64298cb88a6e272a4c98:4:69828161',
